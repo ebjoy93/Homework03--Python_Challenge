728065ed85d493fdbdba2c5b66ec0ca0d87bddbe
ebjoy93 <ebjoy93@gmail.com>
2018-12-09 16:53:33 -0500

    finished PyPoll

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -26,25 +26,47 @@ voteCount = Counter() #voteCount holds the number of votes each canidate receive
 for canidate in canidateNames: #loops through and counts the number of votes each canidate recieves
     voteCount[canidate] += 1
 
-#print(voteCount) #prints how many votes each canidate earned
+print(voteCount)# prints how many votes each canidate earned
 
-##canidateCount = [voteCount]
 votes = []
 for p in voteCount.items():
     votes.append(int(p[1])) #places number of votes each canidate recieved into a list
 #print(votes) #only prints number of votes each canidate received
 
 #convVotes = list(map(int,votes))
-#print(convVotes)
+#print(convVotes) #pulls out the number of votes each canidate receives
 
 #canidateVotes = [int(i) for i in convVotes]
 #intVotes = [int(a) for a in convInt]
 
 votePercent = [p / len(convInt) for p in votes]
-#print(votePercent)
+#print(votePercent) #prints the percent each canidate received
+
+
+winner = voteCount.most_common(1) #finds the canidate with the most votes
+#print(winner)
 
 print("Election Results")
 print("--------------------------------------------------------")
 print("Total Votes: " + str(len(convInt)))
 print("---------------------------------------------------------")
-print(voteCount[0] + votePercent[0])
+print("Number of votes each canidate recieved: " + str(voteCount))
+print("Percent of votes each canidated received: " + str(votePercent))
+print("The winning canidate is :" + str(winner))
+
+newFile = open("pollOutput.txt", "w")
+lines = (   
+            "Election Results", 
+            "-----------------------------",
+            "Total Votes: 3521001",
+            "-----------------------------",
+            "Khan: 63.000% (2218231)",
+            "Correy: 20.000% (704200)",
+            "Li: 14.000% (492940)",
+            "O'Tooley: 3.000% (105630)",
+            "-----------------------------",
+            "Winner: Khan"
+        )
+
+newFile.writelines("\n".join(lines))
+newFile.close()
